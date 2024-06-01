fd1e939adf7ba2d52163ca0e95e43b555e5e2648
Su Hua <hsu@topxgun.com>
2024-06-02 00:23:17 +0800

    提交贮藏修改

--- b.cpp
+++ b.cpp
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-// add a b.cpp file to local main branch
-// git stash main b.cpp
-=======
 // add a b.cpp file to local develop branch
 // fix b.cpp in develop branch,while main branch git stash.
 // fix b.cpp in develop branch,2.
->>>>>>> Stashed changes
+
