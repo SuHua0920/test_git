f09241f1169d28db0e14ca577417b9f5bd1420cd
Su Hua <hsu@topxgun.com>
2024-06-02 01:50:29 +0800

    hebing main

--- b.cpp
+++ b.cpp
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-// add a b.cpp file to local develop branch
-// fix b.cpp in develop branch,while main branch git stash.
-=======
-add a b.cpp file to local main branch
->>>>>>> parent of 4e68c77... git stash 的序号必须被指定
+// add a b.cpp file to local main branch
+// git stash main b.cpp
+//djld 
+// amend
+// test develop
