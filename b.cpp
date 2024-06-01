e077239aa908178b796558d97bf1310f2238332d
Su Hua <hsu@topxgun.com>
2024-06-01 23:55:48 +0800

    接受develop分支上的stash修改

--- b.cpp
+++ b.cpp
@@ -1,3 +1,2 @@
-// add a b.cpp file to local main branch
-// git stash main b.cpp
-// git stash main b.cpp 2
+// add a b.cpp file to local develop branch
+// fix b.cpp in develop branch,while main branch git stash.
