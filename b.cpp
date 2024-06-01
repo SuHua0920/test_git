be3abf4a4ea22f9577f941708a9c7123b485fa14
Su Hua <hsu@topxgun.com>
2024-06-02 01:23:38 +0800

    try delete this commit "提交贮藏修改".
    This reverts commit 40d036b3f10411ac2f504fe593f4db87f3e9f97b.

--- b.cpp
+++ b.cpp
@@ -1,6 +1,2 @@
 // add a b.cpp file to local main branch
 // git stash main b.cpp
-// add a b.cpp file to local develop branch
-// fix b.cpp in develop branch,while main branch git stash.
-// fix b.cpp in develop branch,2.
-
