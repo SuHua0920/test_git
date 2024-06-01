5f1b823287e06dc969c92524fff69a51d7014b9f
Su Hua <hsu@topxgun.com>
2024-06-02 00:12:36 +0800

    Revert "git stash 的序号必须被指定"
    This reverts commit 4e68c773d6e30bf4f3118c68357a3d5add3cf1b0.

--- b.cpp
+++ b.cpp
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 // add a b.cpp file to local develop branch
 // fix b.cpp in develop branch,while main branch git stash.
+=======
+add a b.cpp file to local main branch
+>>>>>>> parent of 4e68c77... git stash 的序号必须被指定
